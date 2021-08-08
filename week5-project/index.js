f45593f736a2a6fabc7ec4aacbef510d046ad887
Joel Johnson <68816736+jjoel1630@users.noreply.github.com>
2021-08-08 14:47:57 -0700

    Create index.js

---
+++ week5-project/index.js
@@ -0,0 +1,8 @@
+const helperFunctions = require('helper.js');
+
+helperFunctions.add(1, 3);
+helperFunctions.subtract(1, 3);
+helperFunctions.multiply(1, 3);
+helperFunctions.divide(1, 3);
+helperFunctions.writeToFile("I a writing this text");
+helperFunctions.readFile("../text.txt");
