f4a2ec0f339ae9e0b65eac0d98cb6723c96cec96
Joel Johnson <68816736+jjoel1630@users.noreply.github.com>
2021-08-08 14:57:37 -0700

    Update index.js

--- week5-project/index.js
+++ week5-project/index.js
@@ -4,5 +4,5 @@ helperFunctions.add(1, 3);
 helperFunctions.subtract(1, 3);
 helperFunctions.multiply(1, 3);
 helperFunctions.divide(1, 3);
-helperFunctions.writeToFile("I a writing this text");
-helperFunctions.readFile("../text.txt");
+helperFunctions.writeToFile("./text.txt", "I a writing this text");
+console.log(helperFunctions.readFile("./text.txt"));
